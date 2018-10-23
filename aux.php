3b3ca6cefdd180b8c56cfe2254acdcd9d8fabf1c
Arturo Barba <arturo.barba@iesdonana.org>
2018-10-23 19:21:38 +0200

    Refactorizo

--- aux.php
+++ aux.php
@@ -1,25 +1,61 @@
 <?php
+/**
+* @author Arturo Barba Rodríguez
+* @copyright Copyright (c) 2018 Arturo Barba Rodríguez
+* @license https://www.gnu.org/licenses/gpl.txt
+*/
+/**
+* Muestra un mensaje de error.
+* @param  string $err El mensaje de error.
+*/
+function mostrarError($err)
+{
+    echo "<h3>Error: $err</h3>";
+}
 function selected($op1, $op2)
 {
-    return $op1 == $op2 ? "selected" : "";
+    return $op1==$op2 ? 'selected' : '';
 }
-function formulario($op1, $op2, $op, $ops)
+/**
+* Muestra el resultado de la operacion.
+* @param  string $p1 Primer operador.
+* @param  string $p2 Segundo operador.
+* @param  string $op Operando.
+*/
+function opera($p1, $p2, $op)
 {
-?>
+    switch ($op) {
+        case '+':
+        $res = $p1+$p2;
+        break;
+        case '-':
+        $res = $p1-$p2;
+        break;
+        case '*':
+        $res = $p1*$p2;
+        break;
+        case '/':
+        $res = $p1/$p2;
+        break;
+        default:
+        mostrarError("fallo de programacion");
+        break;
+    }
+    return $res;
+}
+function form($p1, $p2, $op)
+{ ?>
     <form action="" method="get">
-        <label for="op1">Primer operando *:</label>
-        <input id="op1" type="text" name="op1" value="<?= $op1 ?>"><br/>
-        <label for="op2">Segundo operando *:</label>
-        <input id="op2" type="text" name="op2" value="<?= $op2 ?>"><br/>
-        <label for="op">Operación *:</label>
+        <label for="p1">Primer operando*: </label>
+        <input id="p1" type="text" name="p1" value="<?= $p1 ?>"><br>
+        <label for="p2">Segundo operando*: </label>
+        <input id="p2" type="text" name="p2" value="<?= $p2 ?>"><br>
+        <label for="op">Operacion*: </label>
         <select name="op">
-            <?php foreach ($ops as $o): ?>
-                <option value="<?= $o ?>" <?= selected($op, $o) ?> >
-                    <?= $o ?>
-                </option>
-            <?php endforeach ?>
-        </select><br/>
+            <?php foreach (OP as $o) { ?>
+                <option value="<?= $o ?>" <?= selected($op, $o) ?>><?= $o ?></option>
+            <?php } ?>
+        </select><br>
         <input type="submit" value="Calcular">
     </form>
-<?php
-}
+<?php } ?>
--- index.php
+++ index.php
@@ -1,65 +1,50 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Calculadora</title>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <title>Calculadora</title>
+</head>
+<body>
+    <?php
+    require './aux.php';
+    const OP = ["+", "-", "*", "/"];
+    const PAR = ["op"=>"+", "p1"=>"0", "p2"=>"0"];
+    $p1 = $p2 = $op = null;
+    $err = [];
+
+    // Comprobacion de parametros
+    if (empty($_GET)) {
+        extract(PAR);
+    } elseif (empty(array_diff_key($_GET, PAR)) && empty(array_diff_key($_GET, PAR))) {
+        $_GET = array_map('trim', $_GET);
+        extract($_GET, EXTR_IF_EXISTS);
+    } else {
+        $err[] = "Los parametros recibidos no son correctos.";
+    }
+
+    // Comprobacion de valores
+    if (empty($err)) {
+        if (!is_numeric($p1)) {
+            $err[] = "El primer operando no es un numero.";
+        }
+        if (!is_numeric($p2)) {
+            $err[] = "El segundo operando no es un numero.";
+        }
+        if (!in_array($op, OP)) {
+            $err[] = "El operador no es válido.";
+        }
+    }
+    form($p1, $p2, $op);
+    if (empty($err)) {
+        ?>
+        <h3>Resultado: <?= opera($p1, $p2, $op) ?></h3>
         <?php
-        require './aux.php';
-        const OP = ['+', '-', '*', '/'];
-        const PAR = ['op', 'op1', 'op2'];
-        $op1 = $op2 = $op = null;
-        $error = [];
-        // Comprobación de parámetros:
-        $par = array_keys($_GET);
-        sort($par);
-        if (empty($_GET)) {
-            $op1 = '0';
-            $op2 = '0';
-            $op = '+';
-        } elseif ($par == PAR) {
-            $op1 = trim($_GET['op1']);
-            $op2 = trim($_GET['op2']);
-            $op = trim($_GET['op']);
-        } else {
-            $error[] = "Los parámetros recibidos no son los correctos.";
+    } else {
+        foreach ($err as $me) { ?>
+            <h3>Error : <?= $me ?></h3>
+            <?php
         }
-        if (empty($error)) {
-            // Comprobación de valores:
-            if (!is_numeric($op1)) {
-                $error[] = "El primer operando no es un número.";
-            }
-            if (!is_numeric($op2)) {
-                $error[] = "El segundo operando no es un número.";
-            }
-            if (!in_array($op, OP)) {
-                $error[] = "El operador no es válido.";
-            }
-        }
-        formulario($op1, $op2, $op, OP);
-        if (empty($error)):
-            $res = '';
-            switch ($op) {
-                case '+':
-                    $res = $op1 + $op2;
-                    break;
-                case '-':
-                    $res = $op1 - $op2;
-                    break;
-                case '*':
-                    $res = $op1 * $op2;
-                    break;
-                case '/':
-                    $res = $op1 / $op2;
-                    break;
-            }
-            ?>
-            <h3>Resultado: <?= $res ?></h3>
-        <?php else:
-            foreach ($error as $err): ?>
-                <h3>Error: <?= $err ?></h3>
-            <?php endforeach;
-        endif ?>
-    </body>
+    }
+    ?>
+</body>
 </html>
