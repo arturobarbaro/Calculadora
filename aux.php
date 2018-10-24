d892b214ab3494e60d2d473aec0e26aeefb450af
Arturo Barba <arturo.barba@iesdonana.org>
2018-10-24 20:28:41 +0200

    Refactorizacion y tarea pendiente

--- aux.php
+++ aux.php
@@ -4,14 +4,18 @@
 * @copyright Copyright (c) 2018 Arturo Barba Rodríguez
 * @license https://www.gnu.org/licenses/gpl.txt
 */
+
 /**
 * Muestra un mensaje de error.
 * @param  string $err El mensaje de error.
 */
-function mostrarError($err)
-{
-    echo "<h3>Error: $err</h3>";
+function mostrarErrores($err){
+    foreach ($err as $me): ?>
+        <h3>Error : <?= $me ?></h3>
+        <?php
+    endforeach;
 }
+
 function selected($op1, $op2)
 {
     return $op1==$op2 ? 'selected' : '';
@@ -38,11 +42,19 @@ function calcular($p1, $p2, $op)
         $res = $p1/$p2;
         break;
         default:
-        mostrarError("fallo de programacion");
+        mostrarErrores("fallo de programacion");
         break;
     }
     return $res;
 }
+
+/**
+ * FORMULARIO HTML
+ * @param  [type] $p1 [description]
+ * @param  [type] $p2 [description]
+ * @param  [type] $op [description]
+ * @return [type]     [description]
+ */
 function form($p1, $p2, $op)
 { ?>
     <form action="" method="get">
@@ -58,4 +70,58 @@ function form($p1, $p2, $op)
         </select><br>
         <input type="submit" value="Calcular">
     </form>
-<?php } ?>
+<?php }
+
+/**
+ * Comprueba parametros
+ * @param  [type] $par   [description]
+ * @param  [type] $error [description]
+ * @return [type]        [description]
+ */
+function compruebaParametros($par, &$err){
+    if (!empty($_GET)) {
+        if (empty(array_diff_key($_GET, $par)) &&
+        empty(array_diff_key( $par, $_GET))) {
+            return array_map('trim', $_GET);
+        } else {
+            $err[] = "Los parametros recibidos no son correctos.";
+        }
+    }
+    return $par;
+}
+
+/**
+ * Comprueba valores
+ * @param  [type] $p1  [description]
+ * @param  [type] $p2  [description]
+ * @param  [type] $op  [description]
+ * @param  [type] $ops [description]
+ * @param  [type] $err [description]
+ * @return [type]      [description]
+ */
+function compruebaValores($p1, $p2, $op, $ops, &$err){
+    if (empty($err)) {
+        if (!is_numeric($p1)) {
+            $err[] = "El primer operando no es un numero.";
+        }
+        if (!is_numeric($p2)) {
+            $err[] = "El segundo operando no es un numero.";
+        }
+        if (!in_array($op, $ops)) {
+            $err[] = "El operador no es válido.";
+        }
+    }
+}
+
+/**
+ * [FmostrarResultado description]
+ * @param [type] $p1  [description]
+ * @param [type] $p2  [description]
+ * @param [type] $op  [description]
+ * @param [type] $err [description]
+ */
+function mostrarResultado($p1, $p2, $op, &$err){
+    ?>
+    <h3>Resultado: <?= calcular($p1, $p2, $op) ?></h3>
+    <?php
+}
--- index.php
+++ index.php
@@ -6,44 +6,25 @@
 </head>
 <body>
     <?php
+    //refactorizas para no recibir 3 parametros sino 1 en un array
     require './aux.php';
     const OP = ["+", "-", "*", "/"];
     const PAR = ["op"=>"+", "p1"=>"0", "p2"=>"0"];
-    $p1 = $p2 = $op = null;
+
     $err = [];
 
     // Comprobacion de parametros
-    if (empty($_GET)) {
-        extract(PAR);
-    } elseif (empty(array_diff_key($_GET, PAR)) && empty(array_diff_key($_GET, PAR))) {
-        $_GET = array_map('trim', $_GET);
-        extract($_GET, EXTR_IF_EXISTS);
-    } else {
-        $err[] = "Los parametros recibidos no son correctos.";
-    }
+    extract(compruebaParametros(PAR, $error)); //Crea variables con los contenidos del array
 
     // Comprobacion de valores
-    if (empty($err)) {
-        if (!is_numeric($p1)) {
-            $err[] = "El primer operando no es un numero.";
-        }
-        if (!is_numeric($p2)) {
-            $err[] = "El segundo operando no es un numero.";
-        }
-        if (!in_array($op, OP)) {
-            $err[] = "El operador no es válido.";
-        }
-    }
+    compruebaValores($p1, $p2, $op, OP, $err);
+
     form($p1, $p2, $op);
-    if (empty($err)) {
-        ?>
-        <h3>Resultado: <?= calcular($p1, $p2, $op) ?></h3>
-        <?php
+
+    if (empty($err)){
+        mostrarResultado($p1, $p2, $op, $err);
     } else {
-        foreach ($err as $me) { ?>
-            <h3>Error : <?= $me ?></h3>
-            <?php
-        }
+        mostrarErrores($err);
     }
     ?>
 </body>
